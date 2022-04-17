83412bcc16a4afa4f0325c8297278a9ec190b8f7
nafish08 <md.nafish.sadik@gmail.com>
2022-04-17 15:46:04 +0600

    Navigation bar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
 import './App.css';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header></Header>
     </div>
   );
 }
