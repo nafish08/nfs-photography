85199c3a7b52aa0d19fe766752957f45b99e485c
nafish08 <md.nafish.sadik@gmail.com>
2022-04-17 15:50:43 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
